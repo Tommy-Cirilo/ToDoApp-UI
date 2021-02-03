5ec4b8f3e3081e9b206c18a1a46243143ee8b471
Tommy Cirilo-Thorpe <toperman@macbook-pro.attlocal.net>
2021-02-02 18:45:30 -0600

    created an error component, login component, and welcome component also added routing module

---
+++ ToDoApp-UI/src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username = '';
+  password = '';
+  errorMessage = 'Invalid Credentials';
+  invalidLogin = false;
+
+  //instance of a router
+  // angular.giveMeRouter
+  //Dependency Injection
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  handleLogin() {
+    console.log(this.username);
+    console.log(this.password);
+
+    if(this.username==="tommy" && this.password === "dummy") {
+      //this creates the navigation route for this logical if statement
+      this.router.navigate(['welcome', this.username])
+
+
+      this.invalidLogin = false;
+    } else {
+      this.invalidLogin = true;
+    }
+  }
+}
