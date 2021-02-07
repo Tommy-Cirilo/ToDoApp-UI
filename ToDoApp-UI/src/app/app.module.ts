a985e32a0df78eb41b1825558160584533b5350e
Tommy Cirilo-Thorpe <tommy.v.cirilo@gmail.com>
2021-02-07 13:40:10 -0600

    was able to render the map, but working on making the map a component

--- ToDoApp-UI/src/app/app.module.ts
+++ ToDoApp-UI/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ListTodosComponent } from './list-todos/list-todos.component';
     WelcomeComponent,
     LoginComponent,
     ErrorComponent,
-    ListTodosComponent
+    ListTodosComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
